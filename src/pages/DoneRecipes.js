8ad91e146277231cc629b2e77df48469ea940b69
Cesar Ramos <cesar.2023.ramos@gmail.com>
2022-04-16 16:14:35 -0300

    tela perfil

--- src/pages/DoneRecipes.js
+++ src/pages/DoneRecipes.js
@@ -41,26 +41,54 @@ const DoneRecipes = () => {
 
   return (
     <div>
-      <section>
+      <section className="header_group">
         <Header title="Done Recipes" />
-        <button type="button" onClick={ renderDetails }>All</button>
-        <button type="button" onClick={ filterByFood }>Food</button>
-        <button type="button" onClick={ filterByDrink }>Drinks</button>
-      </section>
-      {favorites.map((item, index) => (
-        <div key={ index }>
-          <input
-            type="image"
-            src={ item.strMealThumb || item.strDrinkThumb }
-            alt="recipe"
-            onClick={ () => redirectTo(item.idDrink || item.idMeal) }
-          />
-          <p>{item.strMeal || item.strDrink}</p>
-          <p>{item.strCategory}</p>
-          <p>{item.strArea || item.strAlcoholic}</p>
-          <p>{item.strTags}</p>
+        <div className="header_buttons">
+          <button
+            className="button_header"
+            type="button"
+            onClick={ renderDetails }
+          >
+            All
+
+          </button>
+          <button
+            type="button"
+            className="button_header"
+            onClick={ filterByFood }
+          >
+            Food
+
+          </button>
+          <button
+            type="button"
+            className="button_header"
+            onClick={ filterByDrink }
+          >
+            Drinks
+
+          </button>
         </div>
-      ))}
+      </section>
+      <div className="card container">
+        {favorites.map((item, index) => (
+          <div key={ index }>
+            <input
+              className="img_details"
+              type="image"
+              src={ item.strMealThumb || item.strDrinkThumb }
+              alt="recipe"
+              onClick={ () => redirectTo(item.idDrink || item.idMeal) }
+            />
+            <h2>{item.strMeal || item.strDrink}</h2>
+            <div style={ { margin: '10px 0' } }>
+              <p>{item.strCategory}</p>
+              <p>{item.strArea || item.strAlcoholic}</p>
+              <p>{item.strTags}</p>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,12 +1,57 @@
 import React from 'react';
+import '../css/Profile.css';
+import { useHistory } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
-const Profile = () => (
-  <div>
-    <Header title="Profile" />
-    <Footer />
-  </div>
-);
+const Profile = () => {
+  const emailStorage = JSON.parse(localStorage.getItem('user'));
+  const history = useHistory();
+
+  const handleLogout = () => {
+    localStorage.clear();
+    history.push('/');
+  };
+
+  return (
+    <div>
+      <Header title="Profile" />
+      <div className="email_profile">
+        <h2 data-testid="profile-email">
+          {' '}
+          {emailStorage.email}
+          {' '}
+        </h2>
+      </div>
+      <section className="buttons_profile">
+        <button
+          type="button"
+          data-testid="profile-done-btn"
+          onClick={ () => history.push('/done-recipes') }
+        >
+          Done Recipes
+
+        </button>
+        <button
+          type="button"
+          data-testid="profile-favorite-btn"
+          onClick={ () => history.push('/favorite-recipes') }
+        >
+          Favorite Recipes
+
+        </button>
+        <button
+          type="button"
+          data-testid="profile-logout-btn"
+          onClick={ handleLogout }
+        >
+          Logout
+
+        </button>
+      </section>
+      <Footer />
+    </div>
+  );
+};
 
 export default Profile;
